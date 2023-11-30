9bb3e8b24c8591526d682ead925b20646ac9db5a
Matelaa <jmatelaneto@gmail.com>
2023-11-30 14:13:36 -0300

    refactoring the profilePicture name and adding Decoder to work with local variables without having to make it optinal

--- software-house-challenge/Home/Model/Video.swift
+++ software-house-challenge/Home/Model/Video.swift
@@ -15,16 +15,32 @@ struct Video: Codable {
     let id: Int
     let songURL: String
     let body: String
-    let profilePictureURL: String
+    let profilePicture: String
     let username: String
     let compressedForIosURL: String
+    var heartCount: Int
+    var fireCount: Int
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(Int.self, forKey: .id)
+        self.songURL = try container.decode(String.self, forKey: CodingKeys.songURL)
+        self.body = try container.decode(String.self, forKey: .body)
+        self.profilePicture = try container.decode(String.self, forKey: CodingKeys.profilePicture)
+        self.username = try container.decode(String.self, forKey: .username)
+        self.compressedForIosURL = try container.decode(String.self, forKey: CodingKeys.compressedForIosURL)
+        self.heartCount = 0
+        self.fireCount = 0
+    }
     
     enum CodingKeys: String, CodingKey {
         case id
         case songURL = "song_url"
         case body
-        case profilePictureURL = "profile_picture_url"
+        case profilePicture = "profile_picture_url"
         case username
         case compressedForIosURL = "compressed_for_ios_url"
+        case heartCount
+        case fireCount
     }
 }
